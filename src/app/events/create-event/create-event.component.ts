828da653c3005b74595c0929567a371cd755d942
Nikhil More <nikhilmore2011@hotmail.com>
2021-02-18 10:57:03 +0100

    updated

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -3,16 +3,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-event',
-  template: `
-  <h1>New Event</h1>
-  <hr>
-  <div class="col-md-6">
-    <h3>[Create Event Form will go here]</h3>
-    <br/>
-    <br/>
-    <button type="submit" class="btn btn-primary">Save</button>
-    <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
-  `,
+  templateUrl: './create-event.component.html',
   styles: [`
   .button {margin-left: 10px;}
   `]
